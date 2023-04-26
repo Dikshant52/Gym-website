32a4e01e9d9d35fc4c93a557c4123ea523e5ba57
Dikshant52 <130077637+Dikshant52@users.noreply.github.com>
2023-04-26 21:20:40 +0530

    Add files via upload

---
+++ checkprice.js
@@ -0,0 +1,19 @@
+
+let price;
+
+
+function getSelectedValue() {
+    var selVal = document.getElementById("package").value;
+var selVal2 = document.getElementById("duration").value;
+var price;
+     console.log(selVal);
+     console.log(selVal2 );
+
+     if(selVal=="Basic"){
+        if(selVal2==monthly){
+            price=1500;
+            document.getElementById('price').innerHTML.value;
+        }
+
+     }
+}
